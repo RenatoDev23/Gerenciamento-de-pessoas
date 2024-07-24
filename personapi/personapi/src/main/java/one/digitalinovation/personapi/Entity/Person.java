002451e03684ac8d234f65473ed370be7fb6ef4a
RenatoDev23 <renatorenatex20@gmail.com>
2024-07-24 20:56:36 -0300

    inicio do projeto gerenciamento de pessoas / Mapeamento das classes

---
+++ personapi/personapi/src/main/java/one/digitalinovation/personapi/Entity/Person.java
@@ -0,0 +1,45 @@
+package one.digitalinovation.personapi.Entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String firsName;
+	
+	@Column(nullable = false)
+	private String lasteName;
+	
+	@Column(nullable = false, unique = true)
+	private String cpf;
+	
+	private LocalDate birthDate;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+	private List<Phone> phones;
+
+
+}
